1607b55b8d0b5971a76b54cdf83adb5680a6b65d
alexheil <aheil4250@gmail.com>
2019-08-30 16:30:39 -0400

    artists and fans

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,24 @@
 class StaticPagesController < ApplicationController
 
+  before_action :redirect_if_signed_in, only: [:register, :sign_in]
+
   def home
   end
+
+  def sign_in
+  end
+
+  def register
+  end
+
+  private
+
+    def redirect_if_signed_in
+      if artist_signed_in? || fan_signed_in?
+        redirect_to root_url
+        flash[:notice] = " You are already signed in!"
+      end
+    end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,23 @@
 Rails.application.routes.draw do
+  devise_for :fans
+  devise_for :artists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "static_pages#home"
+
+  get 'register' => 'static_pages#register'
+  get 'sign-in' => 'static_pages#sign_in'
+
+  devise_for :artists, controllers: { sessions: "artists/sessions", passwords: "artists/passwords", registrations: "artists/registrations", confirmations: "artists/confirmations",  unlocks: "artists/unlocks"}
+
+  devise_scope :artist do
+    get 'sign_out', to: 'artists/sessions#destroy', path: 'sign-out'
+  end
+
+  devise_for :fans, controllers: { sessions: "fans/sessions", passwords: "fans/passwords", registrations: "fans/registrations", confirmations: "fans/confirmations",  unlocks: "fans/unlocks"}
+
+  devise_scope :artist do
+    get 'sign_out', to: 'fans/sessions#destroy', path: 'sign-out'
+  end
 
 end
