d25a66df71566b2cf56401e8b9191b59230398bc
alexheil <aheil4250@gmail.com>
2019-09-07 14:58:21 -0400

    album

--- app/controllers/artists/albums_controller.rb
+++ app/controllers/artists/albums_controller.rb
@@ -32,7 +32,6 @@ class Artists::AlbumsController < ApplicationController
   def edit
     @album = Album.friendly.find(params[:id])
   end
-  
 
   def update
     @album = Album.friendly.find(params[:id])
